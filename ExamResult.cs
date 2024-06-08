18a501d684e505b7c54d6f214cc903f6a184564b
Farida Shakikhanli <turtleowl2005@gmail.com>
2024-06-08 13:08:56 +0400

    modify exam

--- ExamResult.cs
+++ ExamResult.cs
@@ -8,29 +8,12 @@ namespace TestLesson
     public class ExamResult
     {
         
-        //Car bmw = new Car("bmw");
        
         int a = 6;
-        
-        //var,const,dynamic
-        //local vs global variables
-        //int metod
-        //default
-        //ConsoleRead;
-        //const //degerleri almaga mecburdurlar readonly
+      
         public void SeeResult()
         {
-           // private readonly string string;
-//const int first1 = 7;
-             
-            
-            //Console.WriteLine(first1);
-          //  Console.WriteLine(sec2);
-            // var a = 's';
-            //dynamic a = 's';
-            // const int a = 5;
-            //  int a = default;
-            //int a = 5;
+
             char name  = 'd';
              string ad = "123";
             int num = int.Parse(ad);
@@ -52,10 +35,7 @@ namespace TestLesson
             decimal b = 5;
             const int numbers= 34;
      
-            //var d = Console.Read();
-           // var dd;
-           // dynamic set;
-           
+     
       
             double ket = 256;
             byte dee = (byte)ket;
@@ -67,13 +47,7 @@ namespace TestLesson
             int a = 6;//value 
             int b = a;//values
 
-            //object objc = a;//boxing
            
-
-            //int c = (int)objc;//unboxing
-
-            //List<int> aa = new List<int>();
-
 
             
         }
